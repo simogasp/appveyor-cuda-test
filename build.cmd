9008249af55c3424b3f956b427e74dd85130f753
Simone Gasparini <simone.gasparini@gmail.com>
2018-05-30 10:39:29 +0200

    using cuda 9.1 and cudnn 7.0.5

--- build.cmd
+++ build.cmd
@@ -1,16 +1,17 @@
 @echo off
-echo Downloading CUDA toolkit 8
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_windows-exe -FileName cuda_8.0.44_windows.exe
-echo Installing CUDA toolkit 8
-cuda_8.0.44_windows.exe -s compiler_8.0 ^
-                           cublas_8.0 ^
-                           cublas_dev_8.0 ^
-                           cudart_8.0 ^
-                           curand_8.0 ^
-                           curand_dev_8.0
+echo Downloading CUDA toolkit 9
+appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows -FileName cuda_9.1.85_windows.exe
+dir
+echo Installing CUDA toolkit 9
+cuda_9.1.85_windows.exe -s nvcc_9.1 ^
+                           cublas_9.1 ^
+                           cublas_dev_9.1 ^
+                           cudart_9.1 ^
+                           curand_9.1 ^
+                           curand_dev_9.1
 echo Downloading cuDNN
-appveyor DownloadFile https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v5.1/prod/8.0/cudnn-8.0-windows7-x64-v5.1-zip -FileName cudnn-8.0-windows7-x64-v5.1.zip
-7z x cudnn-8.0-windows7-x64-v5.1.zip -ocudnn
+appveyor DownloadFile https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v7.0.5/prod/9.1_20171129/cudnn-9.1-windows7-x64-v7 -FileName cudnn-9.1-windows7-x64-v7
+7z x cudnn-9.1-windows7-x64-v7 -ocudnn
 dir cudnn
 dir cudnn\cuda\bin
 dir cudnn\cuda\lib 
@@ -21,14 +22,14 @@ dir .
 dir "%ProgramFiles%"
 dir "C:\Program Files"
 dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin"
 
-if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin\cudart64_80.dll" ( 
+if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\cudart64_91.dll" ( 
 echo "Failed to install CUDA"
 exit /B 1
 )
 
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v8.0\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\libnvvp;%PATH%
 
 nvcc -V
