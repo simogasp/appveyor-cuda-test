3d9be935c37f187f637cd027352f63fd369f445d
Simone Gasparini <simone.gasparini@gmail.com>
2018-06-10 16:51:17 +0200

    cuda 9.2 and cudnn 7.1

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # appveyor-cuda-test
-Test to install CUDA 9.1 and cudnn 7.0.5 on appveyor [![Build status](https://ci.appveyor.com/api/projects/status/o187npcjrno1x7lg/branch/cuda9.1-cudnn7.0.5?svg=true)](https://ci.appveyor.com/project/simogasp/appveyor-cuda-test-tqvy0/branch/cuda9.1-cudnn7.0.5)
+Test to install CUDA 9.2 and cudnn 7.1 on appveyor [![Build status](https://ci.appveyor.com/api/projects/status/o187npcjrno1x7lg/branch/cuda9.2-cudnn7.1?svg=true)](https://ci.appveyor.com/project/simogasp/appveyor-cuda-test-tqvy0/branch/cuda9.2-cudnn7.1)
--- build.cmd
+++ build.cmd
@@ -1,17 +1,17 @@
 @echo off
-echo Downloading CUDA toolkit 9
-appveyor DownloadFile  https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_windows -FileName cuda_9.1.85_windows.exe
+echo Downloading CUDA toolkit 9.2
+appveyor DownloadFile https://developer.nvidia.com/compute/cuda/9.2/Prod/local_installers/cuda_9.2.88_windows -FileName cuda_9.2.88_windows.exe
 dir
-echo Installing CUDA toolkit 9
-cuda_9.1.85_windows.exe -s nvcc_9.1 ^
-                           cublas_9.1 ^
-                           cublas_dev_9.1 ^
-                           cudart_9.1 ^
-                           curand_9.1 ^
-                           curand_dev_9.1
+echo Installing CUDA toolkit 9.2
+cuda_9.2.88_windows.exe -s nvcc_9.2 ^
+                           cublas_9.2 ^
+                           cublas_dev_9.2 ^
+                           cudart_9.2 ^
+                           curand_9.2 ^
+                           curand_dev_9.2
 echo Downloading cuDNN
-appveyor DownloadFile https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v7.0.5/prod/9.1_20171129/cudnn-9.1-windows7-x64-v7 -FileName cudnn-9.1-windows7-x64-v7
-7z x cudnn-9.1-windows7-x64-v7 -ocudnn
+appveyor DownloadFile https://developer.nvidia.com/compute/machine-learning/cudnn/secure/v7.1.4/prod/9.2_20180516/cudnn-9.2-windows7-x64-v7.1 -FileName cudnn-9.2-windows7-x64-v7.1
+7z x cudnn-9.2-windows7-x64-v7.1 -ocudnn
 dir cudnn
 dir cudnn\cuda\bin
 dir cudnn\cuda\lib 
@@ -22,14 +22,14 @@ dir .
 dir "%ProgramFiles%"
 dir "C:\Program Files"
 dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1"
-dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.2"
+dir "C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin"
 
-if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin\cudart64_91.dll" ( 
+if NOT EXIST "%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin\cudart64_92.dll" ( 
 echo "Failed to install CUDA"
 exit /B 1
 )
 
-set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.1\libnvvp;%PATH%
+set PATH=%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\bin;%ProgramFiles%\NVIDIA GPU Computing Toolkit\CUDA\v9.2\libnvvp;%PATH%
 
 nvcc -V
